adabd96b1bfc163a5dc51a452b5cdf340ebf0bf8
Andrew Lofgren <61328845+andrewlofgren@users.noreply.github.com>
2022-08-30 19:54:31 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Your Name
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Andrew Lofgren\n");
+
+  return 0;
+}
